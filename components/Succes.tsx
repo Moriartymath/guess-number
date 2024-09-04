7b0a0c6c52a13141aca1a74c4195a9f5bbab8dbd
Strelia Illia <seylvin2005@gmail.com>
2024-09-04 20:25:23 +0300

    style success img

--- components/Succes.tsx
+++ components/Succes.tsx
@@ -3,11 +3,11 @@ import { Image, View } from "react-native";
 function Succes() {
   return (
     <View className={"h-full w-full justify-start items-center mt-60"}>
-      <View className={"h-1/4 w-5/6 items-center"}>
+      <View className={"h-1/4"}>
         <Image
           source={require("../assets/images/success.png")}
           resizeMode="cover"
-          className={"h-full w-3/4  rounded-full"}
+          className={"h-full aspect-square rounded-full"}
         />
       </View>
     </View>
